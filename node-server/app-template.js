25e5560050d4d141d2c7198483a5bdad8f50978f
first-repo <61669435+inee1945@users.noreply.github.com>
2025-02-24 17:52:08 +0900

    20250224

---
+++ node-server/app-template.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const fs = require('fs');
+const { type } = require('os');
+const ejs = require('ejs');
+
+const name = 'kims';
+const courses = [
+    {name:'HTML'},{name:'CSS'},{name:'JAVAS'},{name:'NODE'},
+]
+
+const server = http.createServer((req,res)=>{
+    console.log('incomming ....');
+    console.log(req.httpVersion);
+    console.log(req.method);
+    console.log(req.url);
+    const url = req.url;
+    //res.setHeader('Content-Type', 'text/html');
+    if(url==='/'){
+        ejs.renderFile('./template/index.ejs',{name:name}).then((data)=>res.end(data))
+    }else if(url=== '/courses'){
+        ejs.renderFile('./template/courses.ejs',{courses}).then((data)=>res.end(data))
+    }else  {
+        ejs.renderFile('./template/notFound.ejs',{name:name}).then((data)=>res.end(data))
+    }
+  //  res.end();
+});
+
+server.listen(8080);
--- node-server/app.js
+++ node-server/app.js
@@ -5,29 +5,19 @@ const { type } = require('os');
 
 const server = http.createServer((req,res)=>{
     console.log('incomming ....');
-    console.log(req.headers);
     console.log(req.httpVersion);
     console.log(req.method);
     console.log(req.url);
     const url = req.url;
+    //res.setHeader('Content-Type', 'text/html');
     if(url==='/'){
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<header><title>Aacademy</title></header>');
-        res.write('<body><h1>Welcome!</h1></body>');
-        res.write('</html>');
+     fs.createReadStream('./html/index.html').pipe(res);
     }else if(url=== '/courses'){
-         const url =fs.createReadStream('./html/index.html');
-         url.pipe(res);
-         
+        fs.createReadStream('./html/courses.html').pipe(res);
     }else  {
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<header><title>Aacademy</title></header>');
-        res.write('<body><h1>Not Found!</h1></body>');
-        res.write('</html>');
+        fs.createReadStream('./html/notFound.html').pipe(res);
     }
-    res.end();
+  //  res.end();
 });
 
 server.listen(8080);
